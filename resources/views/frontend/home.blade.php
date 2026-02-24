112631760134f787844b7e47ea4863cb20486af1
Rana Bepari <irana.bpr9@gmail.com>
2026-02-24 22:27:17 +0600

    feat: Frontend

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -30,7 +30,7 @@
                       <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
                         <div class="card product-item border-0 mb-4">
                             <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                                <img class="img-fluid w-100" style="height: 120px" src="{{ asset('storage/'.$product->image_path) }}" alt="">
+                                <img class="img-fluid w-100" style="height: 220px" src="{{ asset('storage/'.$product->image_path) }}" alt="">
 
                             </div>
                             <div class="card-body border-left border-right text-center p-0 pt-4 pb-3">
