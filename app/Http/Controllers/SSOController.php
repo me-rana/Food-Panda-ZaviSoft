0cf72cc865c87217f2ecf3fbd29b8c50d75628f2
Rana Bepari <irana.bpr9@gmail.com>
2026-02-25 11:26:34 +0600

    feat: Redirect

--- app/Http/Controllers/SSOController.php
+++ app/Http/Controllers/SSOController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Laravel\Passport\Token;
 
 class SSOController extends Controller
